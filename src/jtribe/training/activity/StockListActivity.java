9b3491c7ce20aca0efb64088ca3f551999b84853
tobias sasse <tosa.info@gmail.com>
2013-10-09 23:56:40 +1100

    Add slide in/out animation for fragments.

--- src/jtribe/training/activity/StockListActivity.java
+++ src/jtribe/training/activity/StockListActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
-public class StockListActivity extends FragmentActivity implements OnStockItemSelectedListener {
+public class StockListActivity extends FragmentActivity implements
+		OnStockItemSelectedListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,14 +17,19 @@ public class StockListActivity extends FragmentActivity implements OnStockItemSe
 
 	@Override
 	public void onStockItemSelected(Uri uri) {
-		StockWebviewFragment webview = (StockWebviewFragment) getSupportFragmentManager().findFragmentById(R.id.stock_webview_fragment);
+		StockWebviewFragment webview = (StockWebviewFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.stock_webview_fragment);
 		if (webview == null || !webview.isInLayout()) {
 			webview = new StockWebviewFragment();
 			Bundle args = new Bundle();
 			args.putString(StockWebviewFragment.LOAD_URL, uri.toString());
 			webview.setArguments(args);
-			FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-			fragmentTransaction.add(R.id.fragment_container, webview, "stockWebview");
+			FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+					.beginTransaction();
+			fragmentTransaction.setCustomAnimations(R.anim.in_right,
+					R.anim.out_right, R.anim.in_right, R.anim.out_right);
+			fragmentTransaction.add(R.id.fragment_container, webview,
+					"stockWebview");
 			fragmentTransaction.addToBackStack("webview");
 			fragmentTransaction.commit();
 		} else {
